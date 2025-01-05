ab6af8e77849394c212005e3c11fbbddf9e8d7a2
yongbeomkwak <yongbeomkwak@gmail.com>
2025-01-05 22:27:56 +0900

    class 및 package 리드미 작성

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,16 @@
 //TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
 // 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-fun main() {
+class Person (
+    val name: String, // 읽기 전용
+    var age: Int
+) {
+    val isMarried: Boolean
+        get() {
+            return age > 40
+        }
 
+}
+
+fun main() {
+    var p = Person("Name", 13)
 }
