b857182299f74b4136146293fb6f9faeca4eb1a4
yongbeomkwak <yongbeomkwak@gmail.com>
2025-03-01 20:15:29 +0900

    주 생성자, 부생성자

--- src/Main.kt
+++ src/Main.kt
@@ -22,8 +22,8 @@ class Sum1: Expr1
 
 fun eval1(e: Expr1): Int {
     return when(e) {
-        is Num1 ->  1
-        is Sum1 ->  2
+        is Num1 ->  10
+        is Sum1 ->  20
         else -> 0
     }
 }
@@ -31,16 +31,25 @@ fun eval1(e: Expr1): Int {
 sealed class Expr2 {
     class Num2: Expr2()
     class Sum2: Expr2()
+    class Kum2: Expr2()
 }
 
 fun eval2(e: Expr2): Int {
     return when(e) {
-        is Expr2.Num2 ->  1
-        is Expr2.Sum2 ->  2
+        is Expr2.Num2 ->  10
+        is Expr2.Sum2 ->  20
+        is Expr2.Kum2 -> 30
     }
 }
 
+// 밖에서 바다올 매개변수 이름과 안에서 사용할 매개변수 이름이 같으면
 class User1(val name: String)
+//class User1(name: String) {
+//    val name: String
+//    init {
+//        this.name = name
+//    }
+//}
 
 class User2 (name: String) {
     private val _name: String
@@ -85,7 +94,7 @@ class User3(name: String, age: Int): SuperUser(name) {
         this._age = age
     }
 
-    constructor(age: Int): this("고정", 10) {
+    constructor(age: Int): this("고정", age) {
         println("User 3 부 생성자 호출")
     }
 
@@ -104,34 +113,26 @@ class User4: SuperUser2 {
 
 
 fun main() {
-    // ✅ NestedClass 인스턴스 생성 (외부 클래스 필요 없음)
-    val nested = OuterClass1.NestedClass()
-    nested.printOuter() // 출력: "바깥쪽에 대한 참조가 없음"
-
-    // ✅ InnerClass 인스턴스 생성 (외부 클래스의 인스턴스 필요)
-    val outer = OuterClass2()
-    val inner = outer.InnerClass()
-    inner.printOuter() // 출력: "3"
-    println("=========================")
-    val n1 = Num1()
-    val s1 = Sum1()
-    println(eval1(n1))
-    println(eval1(s1))
-
-    val n2 = Expr2.Num2()
-    val s2 = Expr2.Sum2()
-    println("=========================")
-    println(eval2(n2))
-    println(eval2(s2))
-
-    val user1 = User1("헬로")
-    println(user1.name)
-
-    val user2 = User2("헬로2")
-    println("_name2") // 접근 불가
-
-    val subUser = SubUser("NN")
-
+//    println("=========================")
+//    val n1 = Num1()
+//    val s1 = Sum1()
+//    println(eval1(n1))
+//    println(eval1(s1))
+//
+//    val n2 = Expr2.Num2()
+//    val s2 = Expr2.Sum2()
+//    println("=========================")
+//    println(eval2(n2))
+//    println(eval2(s2))
+//
+//    val user1 = User1("헬로")
+//    println(user1.name)
+//
+//    val user2 = User2("헬로2")
+//    println("_name2") // 접근 불가
+//
+//    val subUser = SubUser("NN")
+//
     val user3 = User3(20)
 }
 
