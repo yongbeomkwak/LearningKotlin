abfbe57a0a0f7157c30cedd041b1e0655e3c07ce
yongbeomkwak <yongbeomkwak@gmail.com>
2025-04-13 09:50:25 +0900

    object 및 데이터 클래스

--- src/Main.kt
+++ src/Main.kt
@@ -1,38 +1,3 @@
-
-class Temp(val name: String, val age: Int) {
-    override fun toString(): String {
-        return "${name} ${age}"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other == null || other !is Temp) {
-            return false
-        }
-        return name == other.name && age == other.age
-    }
-
-//    override fun hashCode(): Int {
-//        return name.hashCode() * 31 + age
-//    }
-}
-
-data class Client(val name: String, val age: Int)
-
-class CountingSet<T>(
-    val innerSet: MutableCollection<T> = HashSet<T>()
-) : MutableCollection<T> by innerSet { // MutablleCollection의 구현을 innerSet 에게 위임한다.
-    var objectsAdded = 0
-    override fun add(element: T): Boolean { // 위임하지 않고 새로운 구현을 제공
-        objectsAdded++
-        return innerSet.add(element)
-    }
-
-    override fun addAll(c: Collection<T>): Boolean { // 위임하지 않고 새로운 구현을 제공
-        objectsAdded += c.size
-        return innerSet.addAll(c)
-    }
-}
-
 object TempObject {
     val number: Int
 
@@ -41,6 +6,8 @@ object TempObject {
     }
 }
 
+// 선언, 정의
+// 생성, 구현
 class User private constructor(private val nickname: String){ // 주 생성자를 private으로 생성
     companion object { // 동반 객체 선언
         fun newSubscribingUser(email: String) = User(email.substringBefore('@'))
@@ -59,38 +26,33 @@ interface ClickListener {
     fun onClick()
 }
 
+sealed class Expr2 {
+    class Num2: Expr2()
+    class Sum2: Expr2()
+    class Kum2: Expr2()
+}
+
+fun eval2(e: Expr2): Int {
+    return when(e) {
+        is Expr2.Num2 ->  1
+        is Expr2.Sum2 ->  2
+        is Expr2.Kum2 -> 3
+    }
+}
+
+// 선언과 생성 = 인스턴스가 1 개뿐 -> 싱글톤 패턴에 쓰기 좋다
+object Toy {
+    var amount: Int = 0
+}
+
+// 선언
+class CA
 
 fun main() {
-    val t1 = Temp("a", 1)
-    val t2 = Temp("a", 1)
-    println(t1 == t2)
+    var ca1 = CA() // 생성
+    var ca2 = CA() // 생성 , 1 2
 
-    val set = hashSetOf(t1)
-    println(set.contains(t2))
-
-    val c1 = Client("aa", 10)
-    println(c1)
-
-    val copyC2 = c1.copy(age = 20)
-    println(copyC2)
-
-    val cset = CountingSet<Int>()
-    cset.addAll(listOf(1,1,2))
-    println("${cset.objectsAdded} objects were added ${cset.size} remain")
-
-    val subscribingUser = User.newSubscribingUser("bob@gmail.com")
-    val facebookUser = User.newFacebookUser(4)
-
-    val buttonClickListener = object : ClickListener {
-        override fun onClick() {
-            println("버튼이 클릭되었습니다!")
-        }
-    }
-
-    buttonClickListener.onClick()
-
-
-
+    var a = Toy.amount
 }
 
 
