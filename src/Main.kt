1d5028233281d0291bfdae08e0b9ffb33133b033
yongbeomkwak <yongbeomkwak@gmail.com>
2025-05-18 09:00:17 +0900

    람다 프로그래밍, 멤버 참조 + 고차함수

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,32 @@
 import org.w3c.dom.css.Counter
 
+class Bus(speed: Int) {
+    val speedLimit: Int = 50
+
+    private var speed: Int
+
+    init {
+        this.speed = speed
+    }
+
+    fun getSpped(): Int {
+        return speed
+    }
+
+    fun updateSpped(to: Int) {
+        if (to > speedLimit) {
+            return
+        }
+        this.speed = to
+    }
+}
+
+fun printHello() = println("Hello")
+
 fun main() {
-    var number1: Int = 0
-    val incr = { number1 ++}
-
-    incr()
-    class Ref<T>(var value: T)
-
-    val counter = Ref(0)
-    val incr = { counter.value ++}
-    incr()
-
-    println(number1)
+    val original: List<Int> = listOf(1,2,3,4,5,6,7,8,9,10)
+    val new = original
+        .filter { it % 2 == 0 } // 중간 임시 컬렉션 생성
+        .map{ it.toDouble() } // 중간 임시 컬렉션 생성 ..
+    println(new)
 }
