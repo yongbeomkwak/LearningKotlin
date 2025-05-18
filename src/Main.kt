cb4e4135779ac8a4b94180193d6e0df77c44d5a4
yongbeomkwak <yongbeomkwak@gmail.com>
2025-05-18 09:00:17 +0900

    람다 프로그래밍, 멤버 참조 + 고차함수

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,63 @@
 import org.w3c.dom.css.Counter
 
+class Bus(speed: Int) {
+    val speedLimit: Int = 50
+
+    private var speed: Int
+
+    init {
+        this.speed = speed
+    }
+
+    fun getSpped(): Int {
+        return speed
+    }
+
+    fun updateSpped(to: Int) {
+        if (to > speedLimit) {
+            return
+        }
+        this.speed = to
+    }
+}
+
+fun printHello() = println("Hello")
+
+fun withOut(): String {
+    var numbers = mutableListOf<Int>()
+    for (letter in 1..10) {
+        numbers.add(letter)
+    }
+
+    println(numbers)
+    return numbers.toString()
+}
+
+fun  with(): String {
+    var numbers = mutableListOf<Int>()
+
+    with(numbers) {
+        for (letter in 1..10) {
+            add(letter)
+        }
+        return this.toString()
+    }
+}
+
+fun _apply(): List<Int> {
+    var numbers = mutableListOf<Int>()
+    val result = numbers.apply {
+        for (letter in 1..10) {
+            add(letter)
+        }
+    }
+
+    println(result) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    println(numbers) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return result
+}
+
 fun main() {
-    var number1: Int = 0
-    val incr = { number1 ++}
-
-    incr()
-    class Ref<T>(var value: T)
-
-    val counter = Ref(0)
-    val incr = { counter.value ++}
-    incr()
-
-    println(number1)
+    val result = _apply()
+    println(result)
 }
