8ae3d53c238f6b7852d07352cd4b7bb3b2b5afa5
yongbeomkwak <yongbeomkwak@gmail.com>
2025-07-19 21:51:15 +0900

    제네릭 시작

--- src/Main.kt
+++ src/Main.kt
@@ -25,10 +25,29 @@ class Example {
     var message2: String by CustomDelegate()
 }
 
+fun <T,V> abc (param1: T, param2: V): V {
+    return param2
+}
+
+class Processor<T> {
+
+    fun process(value: T) {
+        print(value?.hashCode())
+    }
+}
+
 fun main() {
     val e = Example()
     println(e.message1)
     e.message1 = "안녕"
     e.message2 = "바이"
     println(e.message2)
+
+    print(abc<String,Int>("100",10)) // 타입을 미리 명시
+    print(abc(10,"100")) // 타입 추론
+
+    val processer = Processor<Int>()
+    processer.process(100)
+
+
 }
