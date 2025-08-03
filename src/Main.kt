58d409829dae220358cc0d74433c6c09da13c88d
yongbeomkwak <yongbeomkwak@gmail.com>
2025-08-03 09:08:54 +0900

    델리게이트

--- src/Main.kt
+++ src/Main.kt
@@ -4,50 +4,33 @@ import java.util.LinkedList
 import java.util.Objects
 import javax.print.attribute.standard.MediaSize.Other
 
-class CustomDelegate {
+// Interface
+interface Barista {
+    fun makeCoffee()
+}
 
-    // thisRef: 호출 객체
-    // property: 위임된 프로퍼티 메타데이터
-    operator fun getValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>): String {
-        return "Hello, ${thisRef} '${property.name}' 에서 값을 가져왔어요!"
-    }
-
-    // thisRef: 호출 객체
-    // property: 위임된 프로퍼티 메타데이터
-    // value: 새로 할당된 값
-    operator fun setValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>, value: String) {
-        println("변수 ${thisRef} '${property.name}' 에 '$value' 를 저장했어요.")
+// Delegate
+class StarbucksBarista : Barista {
+    override fun makeCoffee() {
+        println("Make Starbucks-style coffee")
     }
 }
-
-class Example {
-    var message1: String by CustomDelegate()
-    var message2: String by CustomDelegate()
-}
-
-fun <T,V> abc (param1: T, param2: V): V {
-    return param2
-}
-
-class Processor<T> {
-
-    fun process(value: T) {
-        print(value?.hashCode())
+// Delegate
+class EdiyaBarista : Barista {
+    override fun makeCoffee() {
+        println("Make Ediya-style coffee")
     }
 }
 
+// Delegator: Cafe, Delegate: barista
+class Cafe(barista: Barista): Barista by barista
+
+// Usage of Cafe
 fun main() {
-    val e = Example()
-    println(e.message1)
-    e.message1 = "안녕"
-    e.message2 = "바이"
-    println(e.message2)
+    val starbucks = Cafe(StarbucksBarista())
+    starbucks.makeCoffee()
 
-    print(abc<String,Int>("100",10)) // 타입을 미리 명시
-    print(abc(10,"100")) // 타입 추론
-
-    val processer = Processor<Int>()
-    processer.process(100)
-
-
+    val ediya = Cafe(EdiyaBarista())
+    ediya.makeCoffee()
 }
+
