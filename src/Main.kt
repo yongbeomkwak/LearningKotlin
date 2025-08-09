c99e847d5821e35a05e1106e91dda9e9ec0e53f7
yongbeomkwak <yongbeomkwak@gmail.com>
2025-08-09 21:07:29 +0900

    pull

--- src/Main.kt
+++ src/Main.kt
@@ -4,33 +4,71 @@ import java.util.LinkedList
 import java.util.Objects
 import javax.print.attribute.standard.MediaSize.Other
 
-// Interface
-interface Barista {
-    fun makeCoffee()
+//class CustomDelegate {
+//
+//    // thisRef: 호출 객체
+//    // property: 위임된 프로퍼티 메타데이터
+//    operator fun getValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>): String {
+//        return "Hello, ${thisRef} '${property.name}' 에서 값을 가져왔어요!"
+//    }
+//
+//    // thisRef: 호출 객체
+//    // property: 위임된 프로퍼티 메타데이터
+//    // value: 새로 할당된 값
+//    operator fun setValue(thisRef: Any?, property: kotlin.reflect.KProperty<*>, value: String) {
+//        println("변수 ${thisRef} '${property.name}' 에 '$value' 를 저장했어요.")
+//    }
+//}
+//
+//class Example {
+//    var message1: String by CustomDelegate()
+//    var message2: String by CustomDelegate()
+//}
+//
+//fun <T,V> abc (param1: T, param2: V): V {
+//    return param2
+//}
+//
+//class Processor<T> {
+//
+//    fun process(value: T) {
+//        print(value?.hashCode())
+//    }
+//}
+
+class TaxiDriver {
+
 }
 
-// Delegate
-class StarbucksBarista : Barista {
-    override fun makeCoffee() {
-        println("Make Starbucks-style coffee")
-    }
-}
-// Delegate
-class EdiyaBarista : Barista {
-    override fun makeCoffee() {
-        println("Make Ediya-style coffee")
-    }
+interface Talkable {
+    fun talk() {}
 }
 
-// Delegator: Cafe, Delegate: barista
-class Cafe(barista: Barista): Barista by barista
+class  Talker {}
 
-// Usage of Cafe
+class Taxi {
+    val tv: TaxiDriver = TaxiDriver()
+    val tk: Talker = Talker()
+    val a: Int = 0
+}
+
+class  Taxi2 {
+    val tv: TaxiDriver by Talkabl
+}
+
+
 fun main() {
-    val starbucks = Cafe(StarbucksBarista())
-    starbucks.makeCoffee()
+    val e = Example()
+    e.message1 = "12345" // setValue
+    e.message1 = "23455" // setValue
 
-    val ediya = Cafe(EdiyaBarista())
-    ediya.makeCoffee()
+    println(e.message1)
+    e.message2 = "바이"
+    println(e.message2)
+
+
+    val name2: String = ""
+
+
+
 }
-
