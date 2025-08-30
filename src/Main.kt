6c11403f0436ecc3fe2a2a148286e4c05e6f2310
yongbeomkwak <yongbeomkwak@gmail.com>
2025-08-30 19:33:35 +0900

    리

--- src/Main.kt
+++ src/Main.kt
@@ -1,80 +1,13 @@
-//open class Animal {
-//    fun feed(){}
-//}
-//
-//class Herd<out T: Animal> {
-//    val size: Int get() { return 10 }
-//
-//    operator  fun get(i: Int): T {}
-//}
-//
-//fun feedAll(animals: Herd<Animal>) {
-//    for (i in 0 until animals.size) {
-//        animals[i].feed()
-//    }
-//}
-//
-//class  Cat: Animal() {
-//    fun cleanLitter() {}
-//}
-//
-//fun takeCareOfCats(cats: Herd<Cat>) {
-//    for (i in 0 until cats.size) {
-//        cats[i].cleanLitter()
-//    }
-//
-//    feedAll(cats)
-//}
-
-class  ABC {}
-
-fun isABC(value: Any): Boolean {
-    return value is ABC
-}
-
-fun  isInt(value: Any): Boolean {
-    return value is Int
-}
-
-inline fun  <reified T> isT(value: Any): Boolean {
-    return value is T
-}
-
-// 너 T야? T->
-inline fun <reified T> isA(value : Any) = value is T
-
-fun isA(value: Any) = value is String
-
-fun main(args : Array<String>){
-    println(isA<String>("abc")) 		//true
-    println(isA<String>(123))			//false
+interface Consumer<in T> {
+    fun consume(item: T)  // ✅ 가능 (넣는 건 안전)
+    // fun produce(): T ❌ 불가능 (꺼내는 건 위험)
 }
 
 fun main() {
+    val anyConsumer: Consumer<Any> = object : Consumer<Any> {
+        override fun consume(item: Any) = println(item)
+    }
+    val strConsumer: Consumer<String> = anyConsumer
 
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-//    abc()
-
-    println("나 호출됨")
-    println("실행됨")
-    println("실행됨")
-    println("잘못된 코드")
-    println("나 호출됨")
-    println("실행됨")
-    println("실행됨")
-    println("잘못된 코드")
-    println("나 호출됨")
-    println("실행됨")
-    println("실행됨")
-    println("잘못된 코드")
-
+    println(strConsumer.consume("123"))
 }
-
